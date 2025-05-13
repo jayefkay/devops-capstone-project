b52eeec6efcd0493435b0f130f5e1612274bda44
jayefkay <klrjakob@gmail.com>
2025-05-13 09:10:15 -0400

    added link task

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -23,6 +23,8 @@ BASE_URL = "/accounts"
 ######################################################################
 #  T E S T   C A S E S
 ######################################################################
+
+
 class TestAccountService(TestCase):
     """Account Service Tests"""
 
@@ -122,8 +124,6 @@ class TestAccountService(TestCase):
             content_type="test/html"
         )
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
-
-    # ADD YOUR TEST CASES HERE ...
 
     def test_get_account(self):
         """It should Read a single Account"""
